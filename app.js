07aa46dfff279fd8349dffe229abbfc54e674c19
Angel25Novoa <jorge.250495@gmail.com>
2022-03-23 11:32:46 -0600

    canasta

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-
+const agregarVerdura = document.getElementsByClassName('btn-success')
+const valueVerdura = document.getElementById('')
+const agregarFruta = document.getElementsByClassName('btn-info')
+const valueFruta = document.getElementById('')
+const canasta = {}
