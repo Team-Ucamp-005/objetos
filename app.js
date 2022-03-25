a35c14db3b28e2538222b51ccc031d1593778dc5
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-03-25 17:46:29 -0600

    ejercicios

--- app.js
+++ app.js
@@ -1,139 +1,183 @@
-// objetos
+// // objetos
 
-let personaje1 = {
-  "nombre": "Timmy",
-  "raza": "dinosaurio",
-  "edad": 65000000,
-  "peso": 8,
-  "vivo": false,
-  "vamos_a_necesitar_otro_trimmy": function () {
-    if (!this.vivo) {
-      console.log('vamos a necesitar otro timmy!!')
-    }
-  }
+// let personaje1 = {
+//   "nombre": "Timmy",
+//   "raza": "dinosaurio",
+//   "edad": 65000000,
+//   "peso": 8,
+//   "vivo": false,
+//   "vamos_a_necesitar_otro_trimmy": function () {
+//     if (!this.vivo) {
+//       console.log('vamos a necesitar otro timmy!!')
+//     }
+//   }
+// }
+
+// let personaje2 = {
+//   nombre: 'Monkey D. Luffy',
+//   ocupacion: 'pirata',
+//   edad: 19,
+//   transformar: function () {
+//     console.log('Gear fourth... Bounce Man!')
+//   }
+// }
+
+
+// /*
+//   En una recauderia, el vendedor quiere realizar un registro de frutas y verduras que tiene en su local,
+//   y  quiere filtrarlo por tipo: “frutas” o “verduras”, crearemos un objeto por cada fruta y verdura,
+//   en el cual tendrán como propiedades, “nombre”, “tipo”, “cantidad”
+
+//     let item = {
+//       nombre: “Manzana”,
+//       tipo: “fruta”,
+//       cantidad: 5
+//     }
+// */
+
+
+// let canasta = [
+//   {
+//     nombre: 'Manzana',
+//     tipo: 'fruta',
+//     cantidad: 10
+//   },
+//   {
+//     nombre: 'aguacate',
+//     tipo: 'fruta',
+//     cantidad: 10
+//   },
+//   {
+//     nombre: 'sandia',
+//     tipo: 'fruta',
+//     cantidad: 3
+//   },
+//   {
+//     nombre: 'melon',
+//     tipo: 'fruta',
+//     cantidad: 5
+//   },
+//   {
+//     nombre: 'naranja',
+//     tipo: 'fruta',
+//     cantidad: 10
+//   },
+//   {
+//     nombre: 'papa',
+//     tipo: 'verdura',
+//     cantidad: 10
+//   },
+//   {
+//     nombre: 'zanahoria',
+//     tipo: 'verdura',
+//     cantidad: 8
+//   },
+//   {
+//     nombre: 'apio',
+//     tipo: 'verdura',
+//     cantidad: 5
+//   },
+//   {
+//     nombre: 'brocoli',
+//     tipo: 'verdura',
+//     cantidad: 1
+//   },
+//   {
+//     nombre: 'perejil',
+//     tipo: 'verdura',
+//     cantidad: 15
+//   },
+// ]
+// let nuevaCanasta
+
+// // filtra segun el tipo de elemento
+// function filtrarCanasta() {
+//   const tipo = document.getElementById('tipo-filtro').value
+//   nuevaCanasta = canasta.filter(function (element) {
+//     return element.tipo === tipo
+//   })
+//   renderLista(nuevaCanasta)
+// }
+
+
+// // renderiza y muestra la lista de frutas
+// function renderLista(canastaRender) {
+//   const list = document.getElementById('frutas-lista')
+//   list.innerHTML = ''
+//   canastaRender.forEach(function (element, index) {
+//     let node = document.createElement("LI")
+//     let textnode = document.createTextNode(element.nombre + ' ' + element.tipo + ' ' + element.cantidad)
+//     node.setAttribute('id', index)
+//     node.appendChild(textnode)
+//     document.getElementById("frutas-lista").appendChild(node)
+//   });
+
+// }
+
+// // agrega un elemento nuevo a la lista y lo renderiza
+// function agregarFrutaLista() {
+//   const name = document.getElementById('nombre').value
+//   const type = document.getElementById('tipo').value
+//   const quantity = document.getElementById('cantidad').value
+//   if (name === undefined || null || '') {
+//     alert('no ingresaste nada')
+//   } else {
+//     const nuevaCanasta = canasta.slice()
+//     nuevaCanasta.push({
+//       nombre: name,
+//       tipo: type,
+//       cantidad: quantity
+//     })
+//     canasta = nuevaCanasta
+//     renderLista(nuevaCanasta)
+//   }
+// }
+
+
+
+// // renders
+// renderLista(canasta)
+
+
+
+let arr = []
+
+function agregarLibro(autor, title) {
+  arr.push({
+    autor: autor,
+    title: title
+  })
 }
 
-let personaje2 = {
-  nombre: 'Monkey D. Luffy',
-  ocupacion: 'pirata',
-  edad: 19,
-  transformar: function () {
-    console.log('Gear fourth... Bounce Man!')
-  }
+function imrpimirLibros() {
+  console.log(arr)
 }
-
 
 /*
-  En una recauderia, el vendedor quiere realizar un registro de frutas y verduras que tiene en su local, 
-  y  quiere filtrarlo por tipo: “frutas” o “verduras”, crearemos un objeto por cada fruta y verdura, 
-  en el cual tendrán como propiedades, “nombre”, “tipo”, “cantidad”
-
-    let item = {
-      nombre: “Manzana”,
-      tipo: “fruta”,
-      cantidad: 5
-    }
+Dado un string con cualquier palabra, muestra cual es la letra que más se repite en la palabra y la cantidad que aparece en la misma
 */
 
-
-let canasta = [
-  {
-    nombre: 'Manzana',
-    tipo: 'fruta',
-    cantidad: 10
-  },
-  {
-    nombre: 'aguacate',
-    tipo: 'fruta',
-    cantidad: 10
-  },
-  {
-    nombre: 'sandia',
-    tipo: 'fruta',
-    cantidad: 3
-  },
-  {
-    nombre: 'melon',
-    tipo: 'fruta',
-    cantidad: 5
-  },
-  {
-    nombre: 'naranja',
-    tipo: 'fruta',
-    cantidad: 10
-  },
-  {
-    nombre: 'papa',
-    tipo: 'verdura',
-    cantidad: 10
-  },
-  {
-    nombre: 'zanahoria',
-    tipo: 'verdura',
-    cantidad: 8
-  },
-  {
-    nombre: 'apio',
-    tipo: 'verdura',
-    cantidad: 5
-  },
-  {
-    nombre: 'brocoli',
-    tipo: 'verdura',
-    cantidad: 1
-  },
-  {
-    nombre: 'perejil',
-    tipo: 'verdura',
-    cantidad: 15
-  },
-]
-let nuevaCanasta
-
-// filtra segun el tipo de elemento
-function filtrarCanasta() {
-  const tipo = document.getElementById('tipo-filtro').value
-  nuevaCanasta = canasta.filter(function (element) {
-    return element.tipo === tipo
+function letterRepeat(str) {
+  let arr = str.split('')
+  let obj = {}
+  let max = 0
+  let maxLetter = ''
+  arr.forEach(function (element) {
+    if (obj[element] === undefined) {
+      obj[element] = 1
+    } else {
+      obj[element]++
+    }
   })
-  renderLista(nuevaCanasta)
-}
-
-
-// renderiza y muestra la lista de frutas
-function renderLista(canastaRender) {
-  const list = document.getElementById('frutas-lista')
-  list.innerHTML = ''
-  canastaRender.forEach(function (element, index) {
-    let node = document.createElement("LI")
-    let textnode = document.createTextNode(element.nombre + ' ' + element.tipo + ' ' + element.cantidad)
-    node.setAttribute('id', index)
-    node.appendChild(textnode)
-    document.getElementById("frutas-lista").appendChild(node)
-  });
-
-}
-
-// agrega un elemento nuevo a la lista y lo renderiza
-function agregarFrutaLista() {
-  const name = document.getElementById('nombre').value
-  const type = document.getElementById('tipo').value
-  const quantity = document.getElementById('cantidad').value
-  if (name === undefined || null || '') {
-    alert('no ingresaste nada')
-  } else {
-    const nuevaCanasta = canasta.slice()
-    nuevaCanasta.push({
-      nombre: name,
-      tipo: type,
-      cantidad: quantity
-    })
-    canasta = nuevaCanasta
-    renderLista(nuevaCanasta)
+  console.log('pokemon', obj)
+  for (let key in obj) {
+    console.log('pokemon 2', key)
+    if (obj[key] > max) {
+      max = obj[key]
+      maxLetter = key
+    }
   }
+  console.log(maxLetter + ' ' + max)
 }
-
-
-
-// renders
-renderLista(canasta)
+letterRepeat('parangaracutirimicuario')
+letterRepeat('bubbles')
